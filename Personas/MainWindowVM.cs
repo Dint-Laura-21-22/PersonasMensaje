28b2d07bca4e2cc7855bd2ebe18251ed9378ba5d
LauraFernandez <laura.fernandezmol@iesdoctorbalmis.com>
2022-01-20 14:14:06 +0100

    Agregar archivos de proyecto.

---
+++ Personas/MainWindowVM.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Personas
+{
+    class MainWindowVM : ObservableObject
+    {
+        private NavigationService navegacion;
+
+        public RelayCommand AbrirNuevaPersonaCommand { get; }
+        public RelayCommand AbrirListadoPersonasCommand { get; }
+
+        private UserControl contenidoVentana;
+
+        public UserControl ContenidoVentana
+        {
+            get { return contenidoVentana; }
+            set { SetProperty(ref contenidoVentana, value); }
+        }
+
+
+        public MainWindowVM()
+        {
+            navegacion = new NavigationService();
+
+            AbrirListadoPersonasCommand = new RelayCommand(AbrirListadoPersonas);
+            AbrirNuevaPersonaCommand = new RelayCommand(AbrirNuevaPersona);
+        }
+
+        private void AbrirNuevaPersona()
+        {
+            ContenidoVentana = navegacion.ObtenerNuevaPersona();
+        }
+
+        private void AbrirListadoPersonas()
+        {
+            ContenidoVentana = navegacion.ObtenerListadoPersonas();
+        }
+    }
+}
---
+++ Personas/servicios/NavigationService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Personas
+{
+    class NavigationService
+    {
+        private static readonly UserControl listadoPersonas = new ListadoPersonasUserControl();
+
+        public NavigationService()
+        {
+            
+        }
+
+        public UserControl ObtenerNuevaPersona()
+        {
+            return new NuevaPersonaUserControl();
+        }
+
+        public UserControl ObtenerListadoPersonas()
+        {
+            return listadoPersonas;
+        }
+
+        public bool? AbrirDialogoNacionalidad()
+        {
+            DialogoNacionalidad dialogo = new DialogoNacionalidad();
+            return dialogo.ShowDialog();
+        }
+    }
+}
